5e08ce0ce883a6c2e946f50a3949e79094d3bcfb
Nicholas Taboada <21525@columbushs.com>
2019-08-30 15:02:29 -0400

    first commit

---
+++ Class Schedule/src/pkgclass/schedule/ClassSchedule.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgclass.schedule;
+
+/**
+ *
+ * @author 21525
+ */
+public class ClassSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String subject1 = "Begin Weight Training";
+        String teacher1 = "Michael Dukes";
+        String subject2 = "Global Studies";
+        String teacher2 = "Mr.Duggard"; 
+        String subject3 = "Environmental";
+        String teacher3 = "Christopher Connel";
+        String subject4 = "English"; 
+        String teacher4 = "Andrew Chacon";
+        String subject5 = "Algebra 2"; 
+        String teacher5 = "Karl Weber";
+        String subject6 = "American History Accelerated"; 
+        String teacher6 = "Javier Hermida";
+        String subject7 = "Computer Science"; 
+        String teacher7 = "Mr.Larubia";
+        
+   }
+   
+}
